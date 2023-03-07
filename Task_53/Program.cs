112491d559b2bca3502aff9a0feca363df04bb0f
Alex <abreskolenko@yandex.ru>
2023-03-07 11:12:29 +0300

    Add fail task

--- Task_53/Program.cs
+++ Task_53/Program.cs
@@ -11,3 +11,39 @@ int EnterMesseng(string messeng)
 //Создаём размер массива
 var CreateArray = (int sizeCol, int sizeRow) => new int[sizeCol, sizeRow];
 
+// метод для печати двумерного массива
+void PrintArray(int[,] matr)  
+{
+    for (int i = 0; i < matr.GetLength(0); i++)  
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)  
+        {
+            Console.Write($"{matr[i, j]} " + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+//Меняем местами первую и последнюю строку
+string SwapLine(int[,] array)
+{
+    string str = String.Empty;
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            
+        }
+    }
+
+}
+
+//Ввод размера
+int colSize = EnterMesseng("Введите количество строк :");
+int rowSize = EnterMesseng("Введите количество столбцов :");
+
+//Создание массива
+int[,] array = CreateArray(colSize, colSize);
+
+//Распечатываем массив
+PrintArray(array);
