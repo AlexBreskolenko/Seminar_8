425ab606af54a947018acaa01208e42e34ae95d4
Alex <abreskolenko@yandex.ru>
2023-03-07 19:44:27 +0300

    The task 53 is done

--- Task_53/Program.cs
+++ Task_53/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+Console.Clear();
 
 //Ввод числа
 int EnterMesseng(string messeng)
@@ -12,30 +13,47 @@ int EnterMesseng(string messeng)
 var CreateArray = (int sizeCol, int sizeRow) => new int[sizeCol, sizeRow];
 
 // метод для печати двумерного массива
-void PrintArray(int[,] matr)  
+void PrintArray(int[,] arr)
 {
-    for (int i = 0; i < matr.GetLength(0); i++)  
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j = 0; j < matr.GetLength(1); j++)  
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            Console.Write($"{matr[i, j]} " + "\t");
+            Console.Write($"{arr[i, j]} ");
         }
         Console.WriteLine();
     }
 }
 //Меняем местами первую и последнюю строку
-string SwapLine(int[,] array)
+void SwapLine(int[,] array)
 {
-    string str = String.Empty;
-    
-    for (int i = 0; i < array.GetLength(0); i++)
+    int[] arr_1 = new int[array.GetLength(1)];
+    int[] arr_2 = new int[array.GetLength(1)];
+
+    for (int i = 0; i < array.GetLength(1); i++)
     {
-        for(int j = 0; j < array.GetLength(1); j++)
-        {
-            
-        }
+        arr_1[i] = array[0, i];
+        arr_2[i] = array[array.GetLength(0) - 1, i];
     }
 
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        array[0, i] = arr_2[i];
+        array[array.GetLength(0) - 1, i] = arr_1[i];
+    }
+
+}
+
+//Заполняем массив рандомными числами
+void FillRandomArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
 }
 
 //Ввод размера
@@ -43,7 +61,16 @@ int colSize = EnterMesseng("Введите количество строк :");
 int rowSize = EnterMesseng("Введите количество столбцов :");
 
 //Создание массива
-int[,] array = CreateArray(colSize, colSize);
+int[,] array = CreateArray(colSize, rowSize);
+
+//Заполняем рандомными числами
+FillRandomArray(array);
 
 //Распечатываем массив
+PrintArray(array);
+
+Console.WriteLine();
+
+SwapLine(array);
+
 PrintArray(array);
