6b972a1e3eaf70bf717223bb4b2ad7ce7cbbe8fd
Alex <abreskolenko@yandex.ru>
2023-03-07 10:06:42 +0300

    Create task 53

---
+++ Task_53/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+
+
+
+//Ввод числа
+int EnterMesseng(string messeng)
+{
+    Console.WriteLine(messeng);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+//Создаём размер массива
+var CreateArray = (int sizeCol, int sizeRow) => new int[sizeCol, sizeRow];
+
